f921451a74949098e4a2315d22d30b3a405dec25
duymh <d.godcry@gmail.com>
2020-06-09 22:01:05 +0700

    create solution

---
+++ demoReactJs/Controllers/LoginController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace demoReactJs.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : Controller
+    {
+        //private readonly IUnitOfWork _repoDatabase;
+        public LoginController()
+        {
+            //_repoDatabase = repo;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
